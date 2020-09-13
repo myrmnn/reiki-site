2ee245a0658936fd20d539a8bf9e36d3750e75db
myrmnn <myrm.nn@gmail.com>
2020-09-13 14:57:34 -0400

    made edits

--- js/app.js
+++ js/app.js
@@ -6,64 +6,68 @@ hideInitially();
 //navigation
 
 const icon = document.querySelector('.topnav__icon');
-const nav = document.getElementById("myTopnav");
+const nav = document.getElementById('myTopnav');
 const dropbtn = document.querySelectorAll('.topnav__dropbtn');
 const dropcontent = document.querySelectorAll('.topnav__dropdown-content');
 
-dropbtn.forEach(el => el.addEventListener('click', function() {
-  el.nextElementSibling.classList.toggle('topnav__dropdown-content--is-visible');
-  el.firstElementChild.classList.toggle('rotate');
-}))
+dropbtn.forEach((el) =>
+	el.addEventListener('click', function () {
+		el.nextElementSibling.classList.toggle(
+			'topnav__dropdown-content--is-visible'
+		);
+		el.firstElementChild.classList.toggle('rotate');
+	})
+);
 
 icon.addEventListener('click', function () {
-    nav.classList.toggle('responsive');
-    for(i = 0; i < dropcontent.length; i++){
-      dropcontent[i].classList.remove('topnav__dropdown-content--is-visible');
-    }
+	nav.classList.toggle('responsive');
+	for (i = 0; i < dropcontent.length; i++) {
+		dropcontent[i].classList.remove('topnav__dropdown-content--is-visible');
+	}
 });
 
 //Reveal On Scroll
 
 function hideInitially() {
-  itemsToReveal.forEach(el => {
-    el.classList.add('hidden-item');
-    el.isRevealed = false;
-  });
+	itemsToReveal.forEach((el) => {
+		el.classList.add('hidden-item');
+		el.isRevealed = false;
+	});
 }
 
 window.addEventListener('scroll', throttle(calcCaller, 200));
 
 function calculate(el) {
-  if(window.pageYOffset + browserHeight > el.offsetTop){
-    console.log('calculated')
-    let scrollPercent = (el.getBoundingClientRect().y / browserHeight) * 100;
-    if(scrollPercent < 90){
-        el.classList.add('hidden-item--now-visible');
-        el.isRevealed = true;
-    }
-  }
+	if (window.pageYOffset + browserHeight > el.offsetTop) {
+		console.log('calculated');
+		let scrollPercent = (el.getBoundingClientRect().y / browserHeight) * 100;
+		if (scrollPercent < 90) {
+			el.classList.add('hidden-item--now-visible');
+			el.isRevealed = true;
+		}
+	}
 }
 
-function calcCaller(){
-  console.log('scrolled')
-  itemsToReveal.forEach(el => {
-    if(el.isRevealed == false){
-      calculate(el);
-    }
-});
+function calcCaller() {
+	console.log('scrolled');
+	itemsToReveal.forEach((el) => {
+		if (el.isRevealed == false) {
+			calculate(el);
+		}
+	});
 }
 
 function throttle(fn, wait) {
-    var time = Date.now();
-    return function () {
-      if (time + wait - Date.now() < 0) {
-        fn();
-        time = Date.now();
-      }
-    };
+	var time = Date.now();
+	return function () {
+		if (time + wait - Date.now() < 0) {
+			fn();
+			time = Date.now();
+		}
+	};
 }
 
-//form 
+//form
 
 const orange = '#EE6352';
 const inputs = document.querySelectorAll('input');
@@ -73,23 +77,27 @@ const textareas = document.querySelectorAll('textarea');
 const form = document.querySelector('form');
 const main = document.querySelector('main#maincontent');
 
-inputs.forEach(input => input.addEventListener('focus', () => {
-  labels.forEach(label => {
-    label.style.color = 'black';
-  });
-  if (document.activeElement){
-    input.previousElementSibling.style.color = orange;
-  } 
-}))
+inputs.forEach((input) =>
+	input.addEventListener('focus', () => {
+		labels.forEach((label) => {
+			label.style.color = 'black';
+		});
+		if (document.activeElement) {
+			input.previousElementSibling.style.color = orange;
+		}
+	})
+);
 
-textareas.forEach(textarea => textarea.addEventListener('focus', () => {
-  labels.forEach(label => {
-    label.style.color = 'black';
-  });
-  if (document.activeElement){
-    textarea.previousElementSibling.style.color = orange;
-  }
-}))
+textareas.forEach((textarea) =>
+	textarea.addEventListener('focus', () => {
+		labels.forEach((label) => {
+			label.style.color = 'black';
+		});
+		if (document.activeElement) {
+			textarea.previousElementSibling.style.color = orange;
+		}
+	})
+);
 
 // document.addEventListener('click', function(e) {
 // console.log(e.target)
@@ -98,9 +106,9 @@ textareas.forEach(textarea => textarea.addEventListener('focus', () => {
 // }
 // });
 
-document.addEventListener('click', function(e){
-console.log(e.target.className)
-if(e.target.className !== 'input'){
-  labels.forEach(label => label.style.color = 'black')
-}
-})
+document.addEventListener('click', function (e) {
+	console.log(e.target.className);
+	if (e.target.className !== 'input') {
+		labels.forEach((label) => (label.style.color = 'black'));
+	}
+});
